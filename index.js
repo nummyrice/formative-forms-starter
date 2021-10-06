856c4066da02dc298252a696f5e5768ae55917eb
nummyrice <nicholasroyrice@gmail.com>
2021-10-06 11:46:52 -0700

    first phase almost complete

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set("view engine", "pug");
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 const users = [
   {
     id: 1,
@@ -17,6 +11,13 @@ const users = [
     email: "jill.jack@gmail.com"
   }
 ];
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index", { users });
+  // res.send("Hello World!");
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
